8b986e15be6c2369b4580f9d5c170c9c4e97fe42
tzu lin chang <coooo77@hotmail.com>
2023-03-25 22:31:13 +0800

    refactor: user list with getting post

--- src/features/users/UserPage.tsx
+++ src/features/users/UserPage.tsx
@@ -1,24 +1,36 @@
 import { useAppSelector } from '../../app/utils'
 import { selectUserById } from './usersSlice'
-import { selectPostsByUser } from '../posts/postsSlice'
 import { Link, useParams } from 'react-router-dom'
+
+import { type Post, useGetPostsByUserIdQuery } from '../posts/postsSlice'
+import { EntityState } from '@reduxjs/toolkit'
 
 const UserPage = () => {
   const { userId } = useParams()
   const user = useAppSelector((state) => selectUserById(state, userId))
 
-  const postsFromUser = useAppSelector((state) => selectPostsByUser(state, userId))
+  const { data: postsFromUser, isLoading, isSuccess, isError } = useGetPostsByUserIdQuery(userId || '')
 
-  const postTitles = postsFromUser.map((p, i) => (
-    <li key={`${i}_${p.id}`}>
-      <Link to={`/post/${p.id}`}>{p.title}</Link>
-    </li>
-  ))
+  const postToList = (source: EntityState<Post>) => {
+    const { ids, entities } = source
+    return ids.map((id) => (
+      <li key={id}>
+        <Link to={`/post/${id}`}>{entities[id]?.title}</Link>
+      </li>
+    ))
+  }
+
+  // prettier-ignore
+  const content =
+    isLoading ? <p>Loading...</p> :
+    isSuccess ? <ol>{postToList(postsFromUser)}</ol> :
+    isError ? <p>Error occurred</p> :
+    null
 
   return (
     <section>
       <h2>{user?.name}</h2>
-      <ol>{postTitles}</ol>
+      <ol>{content}</ol>
     </section>
   )
 }
